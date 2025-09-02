58be7be00d195a5f13771ff9e97a969e7623bfe2
Ana Livia <analiviadesarocha@gmail.com>
2025-09-02 11:27:48 -0300

    mudança de cmando

--- Atividade01/Atividade01/Program.cs
+++ Atividade01/Atividade01/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Diego!");
+Console.WriteLine("Escreva a altura do retangulo");
+int altura = int.Parse(Console.ReadLine());
+Console.WriteLine("Escreva a base do retangulo");
+int ba = int.Parse(Console.ReadLine());
+int resultado = altura * ba;
+Console.WriteLine($"A area do retangulo é: {resultado}");
+
