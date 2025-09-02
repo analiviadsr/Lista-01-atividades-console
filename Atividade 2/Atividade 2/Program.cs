7f94f4c8f4f1ecc13e083addcf24d1bfa4919f7a
Ana Livia <analiviadesarocha@gmail.com>
2025-09-02 11:42:28 -0300

    criaçao do proframa

--- Atividade 2/Atividade 2/Program.cs
+++ Atividade 2/Atividade 2/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Ola, me fale quantos anos voce tem");
+int anos = int.Parse(Console.ReadLine());
+Console.WriteLine("Escreva quantos meses voce tem");
+int meses = int.Parse(Console.ReadLine());
+Console.WriteLine("Escreva quantos dias voce tem");
+int dias = int.Parse(Console.ReadLine());
+int ano = anos * 365;
+int mes = meses * 30;
+int total = ano + mes + dias;
+Console.WriteLine($"Voce ja viveu { total } dias");
+
+
+
+
