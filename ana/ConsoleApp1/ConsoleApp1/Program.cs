a9360cfc67cb3cdd51a95482a63c678dae59c84a
analiviadsr <analiviadesarocha@gmail.com>
2025-09-09 08:29:28 -0300

    Add files via upload

---
+++ ana/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Olá, escreva um número");
+int numero = int.Parse(Console.ReadLine());
+if (numero > 10)
+{
+    Console.WriteLine("É maior que 10");
+}
+else if (numero < 10)
+{
+    Console.WriteLine("É menor que 10");
+}
+else if (numero == 10)
+{
+    Console.WriteLine("É igual a 10");
+}
+
