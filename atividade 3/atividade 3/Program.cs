071b9bf93c00970c8a3e6b50439ef76bf783029e
Ana Livia <analiviadesarocha@gmail.com>
2025-09-17 08:06:58 -0300

    adicionar atividade 3

---
+++ atividade 3/atividade 3/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Escreva a altura do retângulo");
+int altura = int.Parse(Console.ReadLine());
+Console.WriteLine("Escreva da base do retângulo");
+int ba = int.Parse(Console.ReadLine());
+int resultado = altura * ba;
+Console.WriteLine($"A área do retângulo é {resultado}");
+
